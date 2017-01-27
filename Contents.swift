e5f992f4ee83f7d3207420d2f67e54abbf1a9730
dweil29 <dweil@students.mchenry.edu>
2017-01-27 10:27:05 -0600

    Get_To_Know_MeDakodaW
    Critical thinking exercise that uses constants and variables related to
    me.

---
+++ Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+// This is a creative program using variables and constants
+
+print("This is a program that uses variables and constants related to me!")
+
+var myAge = 19
+// myAge is a variable because it does not stay the same
+
+var favColor = "red"
+// favColor is a variable because it can also change over time
+
+let myName = "Dakoda Klaus Weil"
+// myName is a constant because it will not change. At least not under normal circumstances
+
+var earthAge = 4543000000
+// earthAge is the approximate age of the Earth, 4.543 billion years
+
+print("The Earth is approximately", earthAge - myAge, "years older than me.")
+
+let myHeight = 70
+// my height in inches, as a constant for the purpose of this program(5 foot 10 inches)
+
+let mjHeight = 78
+// Michael Jordans height in inches.(6 foot 6 inches)
+
+print("I am", mjHeight - myHeight, "inches shorter than Michael Jordan.")
